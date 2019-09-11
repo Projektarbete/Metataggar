ebd7d7c9b05ac92af66e9a777eb42e599411fc70
Matilda Björklund <matiildabjorklund@hotmail.com>
2019-09-11 13:12:02 +0200

    .

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function changeBodyBg(color) {
-    refresh.body.style.background = color;
+function randomBackground() {
+    var randomNumber = Math.floor(Math.random() * 10) + 1;
+    var color = "#FFF";
+
+    switch (randomNumber) {
+        case 1:
+            color = #030;
+            break;
+        case 2:
+            color = #300;
+            break;
+    }
+
+    document.body.style.backgroundColor = color;
 }
