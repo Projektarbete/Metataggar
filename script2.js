6628730e95a3b889c82f533354f8d6c8a640f50f
Matilda Björklund <matiildabjorklund@hotmail.com>
2019-09-13 13:16:45 +0200

    .

---
+++ script2.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    function mouse() {
+        let x = document.getElementById('hejdå');
+        console.log(x);
+    }
+    console.log("hej");
+})
+
